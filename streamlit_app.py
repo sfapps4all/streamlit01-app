f76ad678df0318f490158fdffe8f8d28dd51c304
sfapps4all <119809289+sfapps4all@users.noreply.github.com>
2022-12-05 09:27:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as slt
 
-slt.header('Breakfast Menu')
-slt.text('Omega 3 & Blueberry Oatmeal')
-slt.text('Kale, Spinach & Rocket Smoothie')
-slt.text('Hard-Boiled Free-Range Egg')
+slt.header('Breakfast Menu 🥑🍞')
+slt.text('🥣 Omega 3 & Blueberry Oatmeal')
+slt.text('🥗 Kale, Spinach & Rocket Smoothie')
+slt.text('🐔 Hard-Boiled Free-Range Egg')
 slt.text ('© Kale, Spinach')
