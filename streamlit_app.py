3b173257d46510ffcc51ffc7e108cb42f6e19af6
sfapps4all <119809289+sfapps4all@users.noreply.github.com>
2022-12-05 09:44:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,8 @@ lit.text('🥑🍞 Avacado toast')
 
 lit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
