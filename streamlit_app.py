a7952c21cca0874c34e6befbf48e8ee4b01cb322
sfapps4all <119809289+sfapps4all@users.noreply.github.com>
2022-12-10 10:44:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,9 @@ lit.dataframe(fruits_to_show)
 # New Section
 lit.header('Fruityvice fruit advice')
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+fruit_choice = lit.text_input('What fruit would you like information about?','Kiwi')
+lit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #lit.text(fruityvice_response.json())
 
 # write your own comment -what does the next line do? 
