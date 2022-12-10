5b506eb4a34b38072a9727d4752efd75c04a9612
sfapps4all <119809289+sfapps4all@users.noreply.github.com>
2022-12-10 10:37:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,4 +28,4 @@ lit.text(fruityvice_response.json())
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+lit.dataframe(fruityvice_normalized)
