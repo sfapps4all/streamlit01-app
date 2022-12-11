b023b5eafc4e332f04c6951b741b3239d5a9f524
sfapps4all <119809289+sfapps4all@users.noreply.github.com>
2022-12-11 12:12:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as lit
 import pandas as pd
+import snowflake.connector
+
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 lit.header('Breakfast Menu ')
