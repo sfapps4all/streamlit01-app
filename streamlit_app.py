302d89b146a402a87e0327e61181aea6efc7f0d3
sfapps4all <119809289+sfapps4all@users.noreply.github.com>
2022-12-11 13:08:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,17 +26,21 @@ lit.dataframe(fruits_to_show)
 # New Section
 lit.header('Fruityvice fruit advice')
 
+def get_fruitvice_data(fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    #lit.text(fruityvice_response.json())
+    # write your own comment -what does the next line do? 
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+    
+
 try:
   fruit_choice = lit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     lit.error("Please select a fruit to get information.")
   else:    
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-    #lit.text(fruityvice_response.json())
-    # write your own comment -what does the next line do? 
-    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-    # write your own comment - what does this do?
-    lit.dataframe(fruityvice_normalized)
+    fruitvice_response = get_fruitvice_data(fruit_choice)
+    lit.dataframe(fruityvice_response)
 except URLError as e:
   lit.error()
     
